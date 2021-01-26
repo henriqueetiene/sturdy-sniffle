e6142836dbffee4571d544ab2d14c438fc82e183
henriqueetiene <henriqueetienenanideseouza.gmail.com>
2021-01-25 23:12:24 -0400

    adiciona ordenacao na consulta

--- crud/settings.py
+++ crud/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'usuarios'
+    'usuarios.apps.UsuariosConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- usuarios/models.py
+++ usuarios/models.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class Usuario(models.Model):
     nome = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ["nome"]
