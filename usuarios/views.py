ebaa9041018508d9ce4e57b04b6afe7461c5a5d9
henriqueetiene <henriqueetienenanideseouza.gmail.com>
2021-02-01 09:18:03 -0400

    renomeia views

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,13 +4,13 @@ from django.contrib.auth.models import User
 from .serializers import UserSerializer
 
 
-class UsuarioList(generics.ListCreateAPIView):
+class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
-class UsuarioDetail(generics.RetrieveUpdateDestroyAPIView):
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
