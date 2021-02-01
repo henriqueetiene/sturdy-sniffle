57d5ee0b53eff76fb362974d8dbf3b16e86032c3
henriqueetiene <henriqueetienenanideseouza.gmail.com>
2021-02-01 09:11:52 -0400

    testa listagem de usuarios

--- usuarios/test_view.py
+++ usuarios/test_view.py
@@ -11,3 +11,16 @@ class CreateUserTestCase(APITestCase):
     def test_create_user(self):
         response = self.client.post('/usuarios/', {'username': 'henrique'})
         self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+
+
+class ListUsersTestCase(APITestCase):
+    def setUp(self):
+        user = User.objects.create_user(username='admin', password='1234')
+        User.objects.create_user(username='lucas', password='1234')
+        User.objects.create_user(username='joao', password='1234')
+        User.objects.create_user(username='davi', password='1234')
+        self.client.force_login(user)
+
+    def test_list_users(self):
+        response = self.client.get('/usuarios/')
+        self.assertEquals(len(response.json()['results']), 4)
